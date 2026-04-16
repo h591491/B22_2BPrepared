3c5d5d86095a27ae360d176f548986aa74b00b27
h669847 <34232997+Revanitez@users.noreply.github.com>
2026-04-17 01:50:36 +0200

    Test
    Test

---
+++ EmergencyChoiceGame/Assets/Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
