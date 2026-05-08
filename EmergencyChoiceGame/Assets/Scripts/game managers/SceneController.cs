d871124ea19c8d36322b9d63c61927b81dd23e83
h669847 <34232997+Revanitez@users.noreply.github.com>
2026-05-08 02:08:37 +0200

    Changed target scene from TriageFront to 3B to include scene

--- EmergencyChoiceGame/Assets/Scripts/game managers/SceneController.cs
+++ EmergencyChoiceGame/Assets/Scripts/game managers/SceneController.cs
@@ -51,7 +51,7 @@ public class SceneController : MonoBehaviour
             case "3A":
                 if (requirement)
                 {
-                    return "TriageFront";
+                    return "3B";
                 }
                 else
                 {
